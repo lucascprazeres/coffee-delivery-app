75a1940b22da80de6a1a512e55549e7ec8addc76
Lucas Prazeres <lucascprazeres@outlook.com>
2022-09-02 22:17:50 -0300

    style(home): implementa layout mobile-first

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,10 +1,21 @@
 import styled from 'styled-components'
+import { breakpoints } from '../../styles/breakpoints'
 
 export const HomeContainer = styled.div`
   flex: 1;
-  height: calc(100vh - 104px);
+  height: calc(100vh);
   background: url('/src/assets/images/blur-background.png') no-repeat center;
   background-size: cover;
+  padding: 0 2rem;
+
+  @media ${breakpoints.tablet} {
+    height: calc(60vh);
+  }
+
+  @media ${breakpoints.desktop} {
+    height: calc(100vh - 104px);
+    padding: 0;
+  }
 `
 
 export const HomeContent = styled.main`
@@ -17,44 +28,52 @@ export const HeroSection = styled.section`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  gap: 40px;
+  gap: 5rem;
 
-  max-width: 1120px;
+  max-width: 70rem;
   margin: 0 auto;
 
+  img {
+    display: none;
+
+    @media ${breakpoints.desktop} {
+      display: block;
+    }
+  }
+
   h1 {
-    font-size: 48px;
+    font-size: 3rem;
     line-height: 1.3;
 
-    margin-bottom: 16px;
+    margin-bottom: 1rem;
   }
 
   p {
     color: var(--base-subtitle);
-    font-size: 20px;
+    font-size: 1.25rem;
     line-height: 1.3;
   }
 
   ul {
-    margin-top: 66px;
+    margin-top: 4rem;
     list-style: none;
     display: flex;
     align-items: center;
     flex-wrap: wrap;
-    gap: 40px;
+    gap: 2rem;
   }
 `
 
 export const ProductsSection = styled.section`
   height: auto;
 
-  max-width: 1120px;
+  max-width: 70rem;
   margin: 0 auto;
 
   padding-bottom: 5rem;
 
   h2 {
-    font-size: 32px;
+    font-size: 2rem;
     color: var(--base-subtitle);
     margin-bottom: 3rem;
   }
@@ -63,6 +82,12 @@ export const ProductsSection = styled.section`
 export const ProductList = styled.ul`
   display: flex;
   align-items: center;
+  justify-content: center;
   flex-wrap: wrap;
-  gap: 2rem;
+  gap: 4rem;
+
+  @media ${breakpoints.desktop} {
+    justify-content: flex-start;
+    gap: 2rem;
+  }
 `
